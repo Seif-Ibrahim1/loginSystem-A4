df4a795ddb6a85a4d707df210819191bd05e1c98
Youssef Moataz <50296580+YoussefMoataz@users.noreply.github.com>
2022-04-28 14:17:18 +0200

    Update LoginApp.cpp

--- LoginApp.cpp
+++ LoginApp.cpp
@@ -1,3 +1,4 @@
+
 #include "LoginFunctions.cpp"
 
 using namespace std;
@@ -5,8 +6,22 @@ using namespace std;
 int main() {
 
     makeListOfUsers();
+    makeListOfIDs();
 
     getChoices();
+
+//    cout << listUsers[4].password << endl;
+//    cout << listUsers[4].name << endl;
+//    cout << listUsers[4].email;
+
+//    listUsers[0].password = "ay haga";
+//    cout << listUsers[0].password;
+
+//    cout << getUserByID("002").oldPasswords[0] << endl;
+//    cout << getUserByID("002").oldPasswords[1] << endl;
+//    cout << getUserByID("002").oldPasswords[2];
+
+//    login();
 
     return 0;
 }
