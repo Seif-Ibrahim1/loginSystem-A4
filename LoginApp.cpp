063dcd3dc344a17feac6d24bf26169169e90195a
Seif <SE.SeifIbrahim@gmail.com>
2022-04-29 14:06:26 +0200

    Added regex

--- LoginApp.cpp
+++ LoginApp.cpp
@@ -8,6 +8,16 @@ int main() {
 
     getChoices();
 
-}
+//    cout << listUsers[2].password << endl;
+//    cout << listUsers[2].passwordTrials << endl;
+//    cout << listUsers[2].name;
+
+//    listUsers[0].password = "ay haga";
+//    cout << listUsers[0].password;
+
+//    cout << getUserByID("20210").oldPasswords[2];
+
+//    login();
+
     return 0;
 }
